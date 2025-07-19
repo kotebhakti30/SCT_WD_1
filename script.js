bc1557fc77bac018e3e333a2ec5fa558b42df812
kotebhakti30 <kotebhakti@gmail.com>
2025-07-19 16:36:56 +0530

    SCT_WD_1
    My first task as an intern at Skillcraft Technology.

---
+++ script.js
@@ -0,0 +1,11 @@
+// Scroll-based navbar style change
+window.addEventListener("scroll", () => {
+  const navbar = document.getElementById("navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+
+
